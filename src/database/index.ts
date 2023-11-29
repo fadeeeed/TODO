db5f9bc2920a169d93209eaeaf0444ea44186e11
fadeeeed <rupeshsengar123@gmail.com>
2023-11-29 19:30:28 +0530

    Updated start commands

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,17 @@
-import { connect, set } from "mongoose";
-import { MONGODB_USER, MONGODB_PASSWORD, NODE_ENV } from "@/config";
+import { connect, set } from 'mongoose';
+import { MONGODB_USER, MONGODB_PASSWORD, NODE_ENV } from '@/config';
 
 export const dbConnection = async () => {
   const dbConfig = {
-    url: `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@todo.gdeqlge.mongodb.net/?retryWrites=true&w=majority`,
+    url: `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@todo.gdeqlge.mongodb.net/${NODE_ENV}?retryWrites=true&w=majority`,
     options: {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
+      useUnifiedTopology: true
+    }
   };
 
-  if (NODE_ENV !== "production") {
-    set("debug", true);
+  if (NODE_ENV !== 'production') {
+    set('debug', true);
   }
 
   await connect(dbConfig.url);
